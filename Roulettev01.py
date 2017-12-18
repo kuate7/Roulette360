2026a9cf2572e5cef4618b91572edc3d775c8c3e
Carlos Caro <carlos_c75@hotmail.com>
2017-12-17 22:02:47 -0600

    fixed bug that gave more credit when won, and added new features to validate amounts.  Also capped starting balance at 00 based on user entry

--- Roulettev01.py
+++ Roulettev01.py
@@ -1,13 +1,38 @@
 
 import random
 
-#setting initial starting balance for user
-userBalance = 500
 
-#The below code receives bet number and wager wager amount
-print("Welcome to Roulette! Good luck! Your starting balance is $500")
-print("A win on single number will multiply your bet by 37! Good luck!!")
-userBetAmount = input("Please enter a dollar amount to bet: ")
+#Initiates user balance
+userBalance = 0
+userBetAmount = 301
+
+
+
+#Greeting and starting balance.
+print("Welcome to Roulette! You maximum start balance amount is $300. Good luck!")
+
+while userBalance <1 or userBalance > 300:
+    userBalance = int(input("Please enter a starting balance not exceeding $300: "))
+    if userBalance > 300:
+        print("Balance amount exceeds maximum starting of $300.")
+    elif userBalance == 0:
+        print("Starting balance cannot be 0")
+    else:
+        print("Thank you! Your starting balance is " + str(userBalance))
+
+
+#Type of bet - only single number bets are supported at the moment.
+print("A win on a single number will multiply your bet by 37! Good luck!!")
+
+#validate user amount to be is valid
+while userBetAmount > userBalance:
+    userBetAmount= int(input("Please enter a dollar amount to bet: "))
+    if userBetAmount > userBalance:
+        print("You are betting more than you have available.")
+    else:
+        userBalance = userBalance -  userBetAmount
+
+#Start the bet 'single number'
 userNumber = input("Please select a number between 0 and 36, you may also select 00: ")
 
 
@@ -25,6 +50,6 @@ if rouletteSpinNumber == int(userNumber):
     userBalance = userBalance + int(userBetAmount)*37
 else:
     print("You lost! Please try again!")
-    userBalance = userBalance - int(userBetAmount)
+    
 
 print("Your new balance is " + str(userBalance))
