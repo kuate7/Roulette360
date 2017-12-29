fc3f912b248f5e7dd4eb53496486ec2719111fc5
Wendy Burington <wendyburington@gmail.com>
2017-12-28 20:08:41 -0600

    simplifying ==[37]

--- roulette.py
+++ roulette.py
@@ -6,11 +6,9 @@ def rouletteSpin():
     rouletteRange = range(0, 38, 1)
     rouletteSpinNumber = random.sample(rouletteRange, 1)
     if rouletteSpinNumber == [37]:
-        str_rand_item = str('00')
-        rouletteSpinNumber = int(str_rand_item)
+        rouletteSpinNumber = int(0)
     else:
         str_rand_item = str(rouletteSpinNumber).replace('[',"").replace(']',"")
-        print(str_rand_item)
         rouletteSpinNumber = int(str_rand_item)
     return rouletteSpinNumber
 
